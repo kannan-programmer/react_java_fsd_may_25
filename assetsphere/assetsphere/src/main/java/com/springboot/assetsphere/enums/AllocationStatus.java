9fa0d1982959b08c222590be14c61018f24de153
Gopalakannan_N <127022577+kannan-programmer@users.noreply.github.com>
2025-06-29 19:21:50 +0530

    Delete assetsphere directory

--- assetsphere/assetsphere/src/main/java/com/springboot/assetsphere/enums/AllocationStatus.java
+++
@@ -1,6 +0,0 @@
-package com.springboot.assetsphere.enums;
-
-public enum AllocationStatus {
-
-	ASSIGNED, RETURNED, LOST
-}
--- assetsphere/assetsphere/src/main/java/com/springboot/assetsphere/enums/Role.java
+++
@@ -1,6 +0,0 @@
-package com.springboot.assetsphere.enums;
-
-public enum Role {
-
-	HR, EMPLOYEE, IT_SUPPORT
-}
